de67b7f0befa264ac9ecac704515caf5751c3523
김동환 <1033513@naver.com>
2021-06-09 20:53:11 +0900

    plist 카메라, 앨범 접근 권한 요청 앱 개발에 필요한 captureSession, videoDevice , session queue, videoDeviceDiscoverySession 객체 선언

--- MyCamera/ViewController.swift
+++ MyCamera/ViewController.swift
@@ -6,9 +6,19 @@
 //
 
 import UIKit
+import AVFoundation
+import Photos
 
 class ViewController: UIViewController {
 
+    let captureSession = AVCaptureSession()
+    var videoDeviceInput: AVCaptureDeviceInput!
+    let photoOutput = AVCapturePhotoOutput()
+    
+    let sessionQueue = DispatchQueue(label: "session Queue")
+    let videoDeviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInDualCamera, .builtInTripleCamera, .builtInTripleCamera], mediaType: .video, position: .unspecified)
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
